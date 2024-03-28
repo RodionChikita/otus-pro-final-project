6b473e8554db24c1af374ea43882530a78bb90e2
rodioncikita <chikita.rodion@gmail.com>
2024-03-28 20:46:52 +0300

    init

---
+++ demo/src/main/java/ru/otus/java/pro/demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ru.otus.java.pro.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FinalProjectApplication.class);
+	}
+
+}
---
+++ demo/src/test/java/ru/otus/java/pro/demo/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.java.pro.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
